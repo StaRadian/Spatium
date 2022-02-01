ff044c7b1dcc7aac3762df0f029a8e993a6e921d
StaRadian <47676364+StaRadian@users.noreply.github.com>
2022-02-01 09:28:01 +0900

    update

--- src/TexQuard2D.h
+++ src/TexQuard2D.h
@@ -1,23 +1,26 @@
 #pragma once
 
-// struct Vec3
-// {
-//     float x, y, z;
-// };
-
-// struct Vec2
-// {
-//     float x, y;
-// };
-
-// struct Vertex
-// {
-//     Vec3 Position;
-//     Vec2 TexCoords;
-//     float TexID;
-// };
-
-class 2DTexQuard
+struct Vec3
 {
+    float x, y, z;
+};
+
+struct Vec2
+{
+    float x, y;
+};
+
+struct Vertex
+{
+    Vec3 Position;
+    Vec2 TexCoords;
+    float TexID;
+};
+
+class TexQuard2D
+{
+private:
+public:
+    TexQuard2D();
 
 };
